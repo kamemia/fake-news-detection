b94915ab3275df7b77a9ad5c19ab4e71f3ea2c60
Vincent Kamemia <kamemia18@yahoo.com>
2023-01-11 16:00:10 +0300

    Creating a Dashboard

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 
-st.title("Welcome to Fake News Detection!")
+
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):
+    feature_dict = {"No":1,"Yes":2}
+    for key,value in feature_dict.items():
+        if val == key:
+            return value
+
+def get_value(val,my_dict):
+    for key,value in my_dict.items():
+        if val == key:
+            return value
+
+app_mode = st.sidebar.selectbox('Select Page',['Home','Prediction'])
+
+if app_mode=='Home':
+    st.title('Fake News Prediction:')  
+    st.image('Newspaper.jpg')
+    st.markdown('Dataset :')
+    data=pd.read_csv('data/fake.csv')
+    st.write(data.head())
